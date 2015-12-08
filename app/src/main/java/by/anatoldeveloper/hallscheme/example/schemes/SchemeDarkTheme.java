4f446113974291cc56280f6640f1d384409d0478
varyvonchyk.anatol <varyvonchyk.anatol@moneyman.ru>
2015-12-08 19:09:19 +0300

    Fixed example with dark scheme.

--- app/src/main/java/by/anatoldeveloper/hallscheme/example/schemes/SchemeDarkTheme.java
+++ app/src/main/java/by/anatoldeveloper/hallscheme/example/schemes/SchemeDarkTheme.java
@@ -28,12 +28,13 @@ public class SchemeDarkTheme extends Fragment {
         View rootView = inflater.inflate(R.layout.basic_scheme_fragment, container, false);
         rootView.setBackgroundColor(getActivity().getResources().getColor(R.color.dark_grey));
         ZoomableImageView imageView = (ZoomableImageView) rootView.findViewById(R.id.zoomable_image);
-        HallScheme scheme = new HallScheme(imageView, basicScheme(), getActivity());
+        HallScheme scheme = new HallScheme(imageView, schemeWithDifferentColors(), getActivity());
         scheme.setBackgroundColor(getActivity().getResources().getColor(R.color.dark_grey));
         scheme.setScenePosition(ScenePosition.EAST);
         scheme.setScenePaintColor(getActivity().getResources().getColor(R.color.dark_grey));
         scheme.setSelectedSeatTextColor(getActivity().getResources().getColor(R.color.dark_grey));
         scheme.setSceneBackgroundColor(getActivity().getResources().getColor(R.color.white));
+        scheme.setMarkerPaintColor(getActivity().getResources().getColor(R.color.white));
         scheme.setChosenColor(getActivity().getResources().getColor(R.color.white));
         scheme.setSeatListener(new SeatListener() {
 
@@ -51,21 +52,60 @@ public class SchemeDarkTheme extends Fragment {
         return rootView;
     }
 
-    public Seat[][] basicScheme() {
-        Seat seats[][] = new Seat[10][10];
-        for (int i = 0; i < 10; i++)
-            for(int j = 0; j < 10; j++) {
+    public Seat[][] schemeWithDifferentColors() {
+        Seat seats[][] = new Seat[14][12];
+        for (int i = 0; i < 14; i++)
+            for(int j = 0; j < 12; j++) {
                 SeatExample seat = new SeatExample();
                 seat.id = i * 10 + (j+1);
                 seat.selectedSeatMarker = String.valueOf(i+1);
                 seat.status = HallScheme.SeatStatus.BUSY;
                 if (j < 2) {
                     seat.status = HallScheme.SeatStatus.FREE;
+                    seat.color = getActivity().getResources().getColor(R.color.dark_green);
                 }
-                if (i > 3 && i < 6) {
+                if (j == 11) {
+                    seat.status = HallScheme.SeatStatus.FREE;
+                    seat.color = getActivity().getResources().getColor(R.color.dark_purple);
+                }
+                if (i == 0 || i == 13) {
                     seat.status = HallScheme.SeatStatus.EMPTY;
                 }
-                seat.color = getActivity().getResources().getColor(R.color.dark_green);
+                if ((i == 1 || i == 12) && j != 11) {
+                    seat.status = HallScheme.SeatStatus.EMPTY;
+                }
+                if ((i == 2 || i == 11) && (j < 2 || j == 7)) {
+                    seat.status = HallScheme.SeatStatus.EMPTY;
+                }
+                if ((i == 3 || i == 10) && (j < 1 || j == 7)) {
+                    seat.status = HallScheme.SeatStatus.EMPTY;
+                }
+                if ((i == 4 || i == 9) && (j > 7)) {
+                    seat.status = HallScheme.SeatStatus.EMPTY;
+                }
+                if (j == 7) {
+                    seat.status = HallScheme.SeatStatus.EMPTY;
+                }
+                if (i == 0 && j == 11) {
+                    seat.status = HallScheme.SeatStatus.INFO;
+                    seat.marker = String.valueOf(11);
+                }
+                if (i == 1 && (j > 7 && j < 11)) {
+                    seat.status = HallScheme.SeatStatus.INFO;
+                    seat.marker = String.valueOf(j);
+                }
+                if (i == 1 && (j > 1 && j < 7)) {
+                    seat.status = HallScheme.SeatStatus.INFO;
+                    seat.marker = String.valueOf(j+1);
+                }
+                if (i == 2 && j == 1) {
+                    seat.status = HallScheme.SeatStatus.INFO;
+                    seat.marker = String.valueOf(j+1);
+                }
+                if (i == 3 && j == 0) {
+                    seat.status = HallScheme.SeatStatus.INFO;
+                    seat.marker = String.valueOf(j+1);
+                }
                 seats[i][j] = seat;
             }
         return seats;
