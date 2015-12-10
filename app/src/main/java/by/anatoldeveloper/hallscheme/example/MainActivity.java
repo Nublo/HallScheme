1495f8adeed8e9ddce2451652313ac8112d1d7d6
varyvonchyk.anatol <varyvonchyk.anatol@moneyman.ru>
2015-12-10 23:34:34 +0300

    Added menu item with link to github.

--- app/src/main/java/by/anatoldeveloper/hallscheme/example/MainActivity.java
+++ app/src/main/java/by/anatoldeveloper/hallscheme/example/MainActivity.java
@@ -1,9 +1,13 @@
 package by.anatoldeveloper.hallscheme.example;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -27,6 +31,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new ListFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.open_on_github:
+                openOnGithub();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openOnGithub() {
+        Intent openBrowser = new Intent(Intent.ACTION_VIEW);
+        openBrowser.setData(Uri.parse("https://github.com/Nublo/HallScheme"));
+        startActivity(openBrowser);
     }
 
     public static class ListFragment extends Fragment {
