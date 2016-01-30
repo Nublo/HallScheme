310942efc04a78a4a7c489f08f9109f859a47fe9
varyvonchyk-anatol <varyvonchyk.anatol@gmail.com>
2016-01-30 15:04:46 +0300

    Added maximum selected seat example and programmatically click on seat.

--- sample/src/main/java/by/anatoldeveloper/hallscheme/example/MainActivity.java
+++ sample/src/main/java/by/anatoldeveloper/hallscheme/example/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.ListView;
 import by.anatoldeveloper.hallscheme.example.schemes.BasicSchemeFragment;
 import by.anatoldeveloper.hallscheme.example.schemes.SchemeCustomTypeface;
 import by.anatoldeveloper.hallscheme.example.schemes.SchemeDarkTheme;
+import by.anatoldeveloper.hallscheme.example.schemes.SchemeMaxSelectedSeats;
 import by.anatoldeveloper.hallscheme.example.schemes.SchemeWithMarkersFragment;
 import by.anatoldeveloper.hallscheme.example.schemes.SchemeWithSceneFragment;
 
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
     public static class ListFragment extends Fragment {
 
         private static final String[] halls = {"Basic hall scheme", "Scheme with scene", "Scheme with markers",
-                "Custom colors scheme", "Custom typeface"};
+                "Custom colors scheme", "Custom typeface", "Maximum selected seats"};
 
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.halls_fragment, container, false);
@@ -86,6 +87,9 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         case 4 :
                             replaceFragmentAndAddToBackStack(new SchemeCustomTypeface());
+                            break;
+                        case 5 :
+                            replaceFragmentAndAddToBackStack(new SchemeMaxSelectedSeats());
                             break;
                     }
                 }
